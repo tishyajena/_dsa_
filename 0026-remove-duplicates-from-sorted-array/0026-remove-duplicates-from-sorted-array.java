0a30099fe75f2d7186de88e6376eaf238dfe68a6
Tishya Jena <tishyajena719@gmail.com>
2026-05-10 16:27:30 +0530

    .java Easy Array

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,35 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 0;
-        for(int i = 1; i<nums.length; i++){
-            if(nums[i]!=nums[k]){
-                k++;
-                nums[k] = nums[i];
-            }
+    //    int count = 0;
+    //    //if(nums.isEmpty()) return;
+
+    //    int i = 0;
+    //    int j = 1;
+    //    int k = j+1;
+    //    while(j<nums.length){
+    //     if(nums[i] == nums[j]){
+    //         j++;
+    //         i++;
+    //     }
+    //     else{
+    //         nums[i] = nums[j];
+    //         j++;
+    //         count++;
+    //     }
+    //    }
+       
+        int count = 0;
+       HashSet<Integer> set = new HashSet<>();
+       int i = 0;
+       for(int x: nums){
+        if(!set.contains(x)){
+            set.add(x);
+            nums[i++] = x;
+            count++;
         }
-        return k+1;
+        
+       }
+       
+       return count;
     }
 }
