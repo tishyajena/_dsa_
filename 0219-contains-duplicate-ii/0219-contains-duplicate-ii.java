7d56fd4fc9cc13129893621c700c4fcf2f4aeb2a
Tishya Jena <tishyajena719@gmail.com>
2026-05-12 12:58:33 +0530

    .java Easy Array

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i = 0; i<nums.length; i++){
+        
+        for (int i = 0; i<nums.length; i++){
             if(set.contains(nums[i])){
                 return true;
             }
