ad0387f2ddd58fbeb9f3389e20ab52339f00e814
Tishya Jena <tishyajena719@gmail.com>
2026-05-13 22:03:11 +0530

    .java Medium Two Pointers

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,16 +3,13 @@ class Solution {
         String [] a = s.strip().split("\\s+");
         
         StringBuilder sb = new StringBuilder();
-        System.out.println(a[0]);
         for(int i = a.length-1; i>=1; i--){
             sb.append(a[i]);
             sb.append(" ");
         }
         sb.append(a[0]);
         
-        
-        String ans = sb.toString();
-        return ans;
+        return sb.toString();
 
     }
 }
