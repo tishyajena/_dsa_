4fa5e83ce0264d4bb5803a8aa86838d59cb3db7c
Tishya Jena <tishyajena719@gmail.com>
2026-05-14 21:47:28 +0530

    .java Easy Two Pointers

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,15 +2,16 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int h = haystack.length();
         int n = needle.length();
-        
-        for(int i = 0; i<=h-n;i++){
+
+        for(int i = 0; i<=h-n; i++){
             int j = 0;
-            while(j<n && haystack.charAt(i+j)== needle.charAt(j)){
+            while(j<n && haystack.charAt(j+i) == needle.charAt(j)){
                 j++;
                 if(j==n){
                     return i;
                 }
             }
+            
         }
         return -1;
     }
