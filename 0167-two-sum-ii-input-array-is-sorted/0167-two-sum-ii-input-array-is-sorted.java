f28acc284aeef62a118c02b0c0e02d3fb28d3931
Tishya Jena <tishyajena719@gmail.com>
2026-05-14 13:16:18 +0530

    .java Medium Array

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int [] ans = new int[2];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i<numbers.length; i++){
-            map.put(numbers[i],i);
-        }
-        for(int i = 0; i<numbers.length; i++){
-            int com = target-numbers[i];
-            if(map.containsKey(com)){
-                ans[0] = i+1;
-                ans[1] = map.get(com)+1;
-                return ans;
+    
+        int i = 0;
+        int j = numbers.length-1;
+        while(i<j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target){
+                return new int[] {i+1,j+1};
+            }
+            else if(sum>target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
-        return ans;
+        return new int[] {-1,-1};
     }
 }
