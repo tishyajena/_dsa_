989442123bc4e2f4c7fce278468121f0dd8d97ab
Tishya Jena <tishyajena719@gmail.com>
2026-05-19 20:13:03 +0530

    .java Easy Array

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        for(int i = 0; i<nums.length; i++){
+            int sumLeft = 0;
+            int sumRight = 0;
+            for(int j = 0; j<i; j++){
+                sumLeft+=nums[j];
+            }
+            for(int j = i+1; j<nums.length; j++){
+                sumRight+=nums[j];
+            }
+            if(sumLeft == sumRight) return i;
+        } 
+        return -1;
+    }
+}
