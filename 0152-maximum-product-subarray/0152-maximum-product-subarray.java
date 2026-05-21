41a98abbbc43a16ad1ed1d7fa2037be0ff429ba5
Tishya Jena <tishyajena719@gmail.com>
2026-05-21 22:12:31 +0530

    .java Medium Array

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,6 +4,7 @@ class Solution {
         int currProd = 1;
         int maxProd = Integer.MIN_VALUE;
 
+        //check forward once
         for(int i = 0; i<n; i++){
             currProd*=nums[i];
             maxProd = Math.max(currProd, maxProd);
@@ -12,6 +13,7 @@ class Solution {
             }
         }
 
+        //check backward once
         currProd = 1;
         for(int i = n-1; i>=0; i--){
             currProd*=nums[i];
