24f976fbc43d2ef7ffe2e6efa946e95c12176fde
Tishya Jena <tishyajena719@gmail.com>
2026-05-21 19:53:21 +0530

    .java Medium Array

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,27 +2,28 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         Set<List<Integer>> res = new HashSet<>();
-        
-        for(int k = 0; k<nums.length-2; k++){
-            ArrayList<Integer> a = new ArrayList<>();
-            int i = k+1; 
+
+        for(int k =0; k<nums.length-2; k++){
+            int i = k+1;
             int j = nums.length-1;
-            int target = -1*nums[k];
+            int comp = nums[k]*-1;
             while(i<j){
-                int sum = nums[i]+nums[j];
-                if(sum == target){
-                    res.add(Arrays.asList(nums[k],nums[i],nums[j]));
+                if(nums[i]+nums[j]<comp){
                     i++;
-                    j--;
                 }
-                else if(sum<target){
-                    i++;
+                else if (nums[i]+nums[j] > comp){
+                    j--;
                 }
                 else{
+                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    
+                    i++;
                     j--;
                 }
+
             }
         }
-        return new ArrayList<>(res);
+        return new ArrayList<> (res);
+
     }
 }
