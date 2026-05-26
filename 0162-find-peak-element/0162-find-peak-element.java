5adf2483a275a400594f9165b97ac119046ce103
Tishya Jena <tishyajena719@gmail.com>
2026-05-26 22:16:45 +0530

    .java Medium Array

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        if(nums.length == 1) return 0;
+        if(nums[low] > nums[low+1]) return low;
+        if(nums[high]> nums[high-1]) return high;
+
+        low = 1;
+        high = nums.length -2;
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] <nums[mid+1]) {
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+
+}
