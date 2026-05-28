23153456be650655d2e45ecd0dd8741d9e18f6a4
Tishya Jena <tishyajena719@gmail.com>
2026-05-28 13:53:31 +0530

    .java Easy Array

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,42 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+
+        //-----APPROACH 1--------
+        // int n = nums.length;
+        // HashSet<Integer> set = new HashSet<>();
+        // int [] res = new int[2];
+
+        // for(int x: nums){
+        //     if(set.contains(x)){
+        //         res[0] = x;
+        //     }
+        //     set.add(x);
+        // }
+
+        // for(int i = 1; i<=n; i++){
+        //     if(!set.contains(i)){
+        //         res[1] = i;
+        //     }
+        // }
+        // return res;
+
+        //-----APPROACH 2--------
         int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
-        int [] res = new int[2];
-
+        boolean [] seen= new boolean[n+1];
+        int duplicate = 0;
+        int missing = 0;
         for(int x: nums){
-            if(set.contains(x)){
-                res[0] = x;
+            if(seen[x]){
+                duplicate = x;
             }
-            set.add(x);
+            seen[x] = true;
         }
-
         for(int i = 1; i<=n; i++){
-            if(!set.contains(i)){
-                res[1] = i;
+            if(!seen[i]){
+                missing = i;
+                break;
             }
         }
-        return res;
+        return new int[] {duplicate, missing};
     }
 }
