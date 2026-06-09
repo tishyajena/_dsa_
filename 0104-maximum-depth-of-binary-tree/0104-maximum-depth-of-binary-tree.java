5984dad834b180ad5032037934fcf4013c86ff78
Tishya Jena <tishyajena719@gmail.com>
2026-06-09 13:45:10 +0530

    .java Easy Tree

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -16,9 +16,10 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
+
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
 
-        return Math.max(leftDepth, rightDepth) + 1;
+        return Math.max(leftDepth,rightDepth)+1;
     }
 }
