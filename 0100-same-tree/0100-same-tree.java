c318e1912987bd010e3b55ddc7b84fe125b85c1b
Tishya Jena <tishyajena719@gmail.com>
2026-06-09 13:29:33 +0530

    .java Easy Tree

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -5,7 +5,7 @@
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
+ *     T/reeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
@@ -15,30 +15,31 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        //------BFS------
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.offer(p);
+        // queue.offer(q);
 
-        //--------BFS------------
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(p);
-        queue.offer(q);
-        while(!queue.isEmpty()){
-            TreeNode p1 = queue.poll();
-            TreeNode q1 = queue.poll();
+        // while(!queue.isEmpty()){
+        //     TreeNode p1 = queue.poll();
+        //     TreeNode q1 = queue.poll();
 
-            if(p1 == null && q1 == null) continue;
-            if(p1 == null || q1 == null) return false;
-            if(p1.val != q1.val) return false;
+        //     if(p1 == null && q1 == null) continue;
+        //     if(p1 == null || q1 == null) return false;
+        //     if(p1.val != q1.val) return false;
 
-            queue.offer(p1.left); queue.offer(q1.left);
-            queue.offer(p1.right); queue.offer(q1.right);
-        }
-        
-        return true;
+        //     queue.offer(p1.left); queue.offer(q1.left);
+        //     queue.offer(p1.right); queue.offer(q1.right);
+        // }
 
-        //-----------DFS--------------
-        // if(p == null && q == null) return true;
-        // if(p == null || q == null) return false;
+        // return true;
 
-        // return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        //-----DFS------
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+
+        return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+
 
     }
 }
