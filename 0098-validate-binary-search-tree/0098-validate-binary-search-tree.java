b7c5329002592bc687561d62ae02aa2a28b06564
Tishya Jena <tishyajena719@gmail.com>
2026-06-10 22:24:55 +0530

    .java Medium Tree

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,22 +14,37 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        //----INORDER --> SORTED----
-        List<Integer> inOrderList = new LinkedList<>();
-        helper(root, inOrderList);
+    //----ITERATIVE APPROACH-----
+    //----INORDER --> SORTED----
 
-        int prev = inOrderList.get(0);
-        for(int i = 1; i<inOrderList.size(); i++){
-            if(inOrderList.get(i)<=prev) return false;
-            prev = inOrderList.get(i);
-        }
-        return true;
+    // public boolean isValidBST(TreeNode root) {     
+    //     List<Integer> inOrderList = new LinkedList<>();
+    //     helper(root, inOrderList);
+
+    //     int prev = inOrderList.get(0);
+    //     for(int i = 1; i<inOrderList.size(); i++){
+    //         if(inOrderList.get(i)<=prev) return false;
+    //         prev = inOrderList.get(i);
+    //     }
+    //     return true;
+    // }
+    // private void helper(TreeNode root, List<Integer> p){
+    //     if(root == null) return;
+    //     helper(root.left, p);
+    //     p.add(root.val);
+    //     helper(root.right, p);
+    // }
+
+    //-----RECURSIVELY------
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
     }
-    private void helper(TreeNode root, List<Integer> p){
-        if(root == null) return;
-        helper(root.left, p);
-        p.add(root.val);
-        helper(root.right, p);
+    private boolean validate(TreeNode node, Integer low, Integer high){
+        if(node == null) return true;
+        if(low != null && node.val<= low) return false;
+        if(high != null && node.val>= high) return false;
+
+        return validate(node.left, low, node.val) && validate(node.right, node.val, high);
     }
+
 }
