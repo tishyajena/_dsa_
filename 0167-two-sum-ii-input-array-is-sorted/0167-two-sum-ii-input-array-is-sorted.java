067eda840e3fd38b62ff02a7385db033c5a7c6e1
Tishya Jena <tishyajena719@gmail.com>
2026-06-15 20:23:03 +0530

    .java Medium Array

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,18 +3,19 @@ class Solution {
     
         int i = 0;
         int j = numbers.length-1;
-        while(i<j){
-            int sum = numbers[i] + numbers[j];
+
+        while(i != j){
+            int sum = numbers[j] + numbers[i];
             if(sum == target){
-                return new int[] {i+1,j+1};
+                break;
             }
-            else if(sum>target){
-                j--;
-            }
-            else{
+            else if(sum<target){
                 i++;
             }
+            else{
+                j--;
+            }
         }
-        return new int[] {-1,-1};
+        return new int[] {i+1,j+1};
     }
 }
