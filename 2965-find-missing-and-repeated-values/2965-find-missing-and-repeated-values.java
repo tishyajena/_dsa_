c588ac505805e164451c029c7f8f53f764d33b1b
Tishya Jena <tishyajena719@gmail.com>
2026-06-17 22:14:27 +0530

    .java Easy Array

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,23 +1,48 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashSet<Integer> set = new HashSet<>();
-        int m = grid.length;
+        // HashSet<Integer> set = new HashSet<>();
+        // int m = grid.length;
+        // int n = grid[0].length;
+
+        // int [] res = new int[2];
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(set.contains(grid[i][j])){
+        //             res[0] = grid[i][j];
+        //         }
+        //         set.add(grid[i][j]);
+        //     }
+        // }
+        // for(int i = 1; i<=m*n; i++){
+        //     if(!set.contains(i)){
+        //         res[1] = i;
+        //     }
+        // }
+        // return res;
+
+        int m= grid.length;
         int n = grid[0].length;
 
-        int [] res = new int[2];
+        int [] arr = new int[(m*n)+1];
+
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(set.contains(grid[i][j])){
-                    res[0] = grid[i][j];
-                }
-                set.add(grid[i][j]);
+                arr[grid[i][j]]++;
             }
         }
-        for(int i = 1; i<=m*n; i++){
-            if(!set.contains(i)){
-                res[1] = i;
+
+        int missing = -1;
+        int repeated = -1;
+
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] == 0){
+                missing = i;
+            }
+            else if(arr[i] == 2){
+                repeated = i;
             }
         }
-        return res;
+
+        return new int[] {repeated, missing};
     }
 }
