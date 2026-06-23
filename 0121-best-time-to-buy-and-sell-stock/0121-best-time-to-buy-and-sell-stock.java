a72f51bf75f7fe9480b4b282f64d7f07b52e585b
Tishya Jena <tishyajena719@gmail.com>
2026-06-23 13:47:22 +0530

    .java Easy Array

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,30 +2,20 @@ class Solution {
     
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        // int buy = prices[0];
-        // int profit = 0;
-        // for(int i = 1; i<n; i++){
-        //     if(prices[i]<buy){
-        //         buy = prices[i];
-        //     }
-        //     profit = Math.max(profit, prices[i]-buy);
-        // }
-        // return profit;
-        int i = 0;
+        int i =0;
         int j = 1;
         int maxSum = 0;
+
         while(j<n){
             if(prices[i]<prices[j]){
                 maxSum = Math.max(maxSum, prices[j]-prices[i]);
                 j++;
             }
-        
             else{
                 i = j;
                 j++;
             }
         }
         return maxSum;
-        
     }
 }
