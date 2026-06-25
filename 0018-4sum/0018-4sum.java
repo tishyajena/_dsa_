9052557743cfb19737d219013c2e865c48c5a30d
Tishya Jena <tishyajena719@gmail.com>
2026-06-25 22:18:19 +0530

    .java Medium Array

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        Set<List<Integer>> res = new HashSet<>();
         int n = nums.length;
-        for (int i = 0; i<n-3; i++){
-            for(int j = i+1; j<n-2; j++){
-                long sum = nums[i] + nums[j];
-                int left = j+1;
-                int right = n-1;
-                while(left<right){
-                    long sum1 = nums[left] + nums[right] + sum;
-                    if(sum1 == target){
-                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left++;
-                        right--;
-                    }
-                    else if (sum1>target){
-                        right--;
-                    }
-                    else{
-                        left++;
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < n - 3; i++) {
+            for (int j =i+1; j < n - 2; j++) {
+                long comp = (long)target - (nums[i] + nums[j]);
+                int a = j + 1;
+                int b = n-1;
+                while (a < b) {
+                    int sum = nums[a] + nums[b];
+
+                    if (sum == comp) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[a], nums[b]));
+                        a++;
+                        b--;
+                    } else if (sum > comp) {
+                        b--;
+                    } else {
+                        a++;
                     }
                 }
-            }
+                
 
+            }
         }
-        return new ArrayList<>(res);
+
+        return new ArrayList<>(result);
     }
 }
