91707665c86f97ab3c5f81f875f57f0c19bfc0fa
Tishya Jena <tishyajena719@gmail.com>
2026-07-23 21:37:55 +0530

    .java Medium Hash Table

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,8 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashSet<Character> set = new HashSet<>();
-        // int maxLen = 0;
-        // for(int i = 0; i<s.length(); i++){
-        //     int l = 0;
-        //     for(int j = i; j<s.length();j++){
-        //         if(set.contains(s.charAt(j))){
-        //             break;
-        //         }
-        //         else{
-        //             set.add(s.charAt(i));
-        //         }
-        //         l++;
-        //     }
-        //     maxLen = Math.max(maxLen, l);
-        // }
-        // return maxLen;
-
-        int count =0;
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
         int left = 0;
-        Set<Character> set = new HashSet<>();
 
         char[] ch = s.toCharArray();
 
@@ -32,6 +15,7 @@ class Solution {
             set.add(ch[right]);
 
             count = Math.max(count, right-left+1);
+    
         }
         return count;
     }
