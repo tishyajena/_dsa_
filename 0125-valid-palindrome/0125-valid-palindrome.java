19693825eced6f96493c101d2ca6e50c8198e22e
Tishya Jena <tishyajena719@gmail.com>
2026-08-05 19:26:39 +0530

    .java Easy Two Pointers

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        for (char ch : s.toCharArray()) {
-            if(Character.isLetter(ch) || Character.isDigit(ch)){
-                char x = Character.toLowerCase(ch);
-                sb.append(x);
+        for(char x: s.toCharArray()){
+            if(Character.isLetter(x) || Character.isDigit(x)){
+                sb.append(Character.toLowerCase(x));
             }
         }
         int i = 0;
         int j = sb.length()-1;
+
         while(i<j){
             if(sb.charAt(i) != sb.charAt(j)){
                 return false;
             }
-            i++; j--;
+            i++;
+            j--;
         }
+
+
         return true;
     }
 }
